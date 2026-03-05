2cda251b5904f4caad75a7e1edf6bcffaec2d411
s0u1drinker <norkiw40k@gmail.com>
2026-03-05 18:51:06 +0300

    feat: Header: Добавлена кнопка выхода

--- apps/web/app/components/Header.vue
+++ apps/web/app/components/Header.vue
@@ -1,7 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { PAGE_PATH } from '@constants';
+
+  /** Завершение сеанса. */
+  const logoutUser = () => {
+    userLogOut();
+    navigateTo(PAGE_PATH.login);
+  };
+</script>
 
 <template>
-  <header class="header">Header</header>
+  <header class="header">
+    Header
+    <VscButton
+      text="Выход"
+      button-style="plain"
+      @click="logoutUser"
+    />
+  </header>
 </template>
 
 <style scoped></style>
